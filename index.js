e6b5c097922f80f06e981762ccb5566081abc09a
Ankush Kapoor <ankushkapoor179@gmail.com>
2023-02-21 22:15:04 +0530

    initial node setup

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get("/home", (req, res) => {
+//   res.send("<h1>Server is running on 8000</h1>");
+// });
+
+// app.get("/info", (req, res) => {
+//   res.json({ name: "ABC", age: 20 });
+// });
+
+// // Headers, body, params/urlparams , query/queryparams
+
+// app.post("/addUser", (req, res) => {
+//   console.log(req.headers['access-token'])
+//   res.json(req.body);
+// });
+
+let productsArr = ["Iphone", "Laptop", "Smart Watch", "Headphones"];
+
+app.post("/addProduct", (req, res) => {
+  const productToBeAdded = req.body.product;
+  console.log(productToBeAdded);
+  productsArr.push(productToBeAdded);
+  res.json({ message: "Product added Successfully" });
+});
+
+app.delete("/delete/:name", (req, res) => {
+  // You will remove 2 elements from array start
+  // and 2 elements from index 2
+  // If we want to remove based on name of product
+
+  console.log(req.params.name);
+  const filteredArr = productsArr.filter((elem) => elem !== req.params.name);
+  productsArr = filteredArr;
+  res.json({ message: "Product deleted successfully" });
+});
+
+app.put("/update/:index", (req, res) => {
+  const idx = req.params.index;
+  const newProduct = req.body.product;
+  // productsArr.splice(idx, 1, newProduct);
+  productsArr[idx] = newProduct
+  res.json({ message: "Product updated Successfully" });
+});
+
+app.get("/products", (req, res) => {
+  res.json({ products: productsArr });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
